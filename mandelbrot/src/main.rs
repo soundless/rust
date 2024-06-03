3ea6f8bd9718e41a3ac4856fdd04171a161886b6
HengJun Tian <hengjun.tian@gmail.com>
2024-06-02 22:45:31 -0700

    fix: add all the files

---
+++ mandelbrot/src/main.rs
@@ -0,0 +1,53 @@
+use num::Complex;
+use std::str::FromStr;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn complex_square_add_loop(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i)
+        }
+        z = z * z + c;
+    }
+    None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index+1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("",','), None);
+    assert_eq!(parse_pair::<i32>("10,",','), None);
+    assert_eq!(parse_pair::<i32>(",10",','), None);
+    assert_eq!(parse_pair::<i32>("10,20",','), Some((10,20)));
+    assert_eq!(parse_pair::<i32>("10,20xy",','), None);
+    assert_eq!(parse_pair::<f64>("0.5x",'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5",'x'), Some((0.5,1.5)));
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex{re, im}),
+        None => None
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"), Some(Complex{re: 1.25, im: -0.0625}));
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
